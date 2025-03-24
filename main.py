ad8d933697352f43b92c289952ff6ddf9622ac86
scp220 <scp220@lehigh.edu>
2025-03-24 13:09:52 -0400

    added hessian tests

--- main.py
+++ main.py
@@ -37,16 +37,17 @@ assert jnp.isclose(event_times[permutation], event_times_permute).all()
 
 # checking automatic differentiation gradients with finite difference
 
-def objective(x, p):
+def objective(x, t, p):
+    time_span = jnp.linspace(0, t, 100)
     xinit = jnp.tile(x, (10, 1)) + jrandom.normal(key, shape = (10, 2)) * 0.1
     solution = odeint_event(afunc, event_vmap, transfer, xinit, time_span, p)
     return jnp.mean((solution[0])**2)
 
 
 # check gradients using finite difference
-def gradient_fd(x, p, eps):
+def gradient_fd(x, t, p, eps):
 
-    args_flatten, unravel = flatten_util.ravel_pytree(( x, p ))
+    args_flatten, unravel = flatten_util.ravel_pytree(( x, t, p ))
     def _grad(v):
         loss = objective(*unravel(args_flatten + eps * v)) - objective(*unravel(args_flatten - eps * v))
         return loss / 2 / eps
@@ -56,5 +57,20 @@ def gradient_fd(x, p, eps):
 
 
 x = xinit[0]
-gradient_x, gradient_p = jax.grad(objective, argnums = (0, 1))(x, p)
-fd_x, fd_p = gradient_fd(x, p, 1e-4)
+t = jnp.array(5.)
+
+# Testing gradients
+gradient_x, gradient_t, gradient_p = jax.grad(objective, argnums = (0, 1, 2))(x, t, p) # stop gradient issues
+print("Gradient reverse mode autodiff", f"gradient x : {gradient_x}", f"gradient t : {gradient_t}", f"gradient p : {gradient_p}", sep = "\n")
+gradient_x, gradient_t, gradient_p = jax.jacfwd(objective, argnums = (0, 1, 2))(x, t, p)
+print("Gradient reverse mode autodiff", f"gradient x : {gradient_x}", f"gradient t : {gradient_t}", f"gradient p : {gradient_p}", sep = "\n")
+fd_x, fd_t, fd_p = gradient_fd(x, t, p, 1e-4)
+print("Gradient finite difference", f"gradient x : {fd_x}", f"gradient t : {fd_t}", f"gradient p : {fd_p}", sep = "\n")
+
+# Testing hessians
+hess = jax.hessian(objective, argnums = (0, 1, 2))(x, t, p)
+print("Hessian fwd(rev) : ", hess, sep = "\n")
+hess_fwd = jax.jacfwd(jax.jacfwd(objective, argnums = (0, 1, 2)), argnums = (0, 1, 2))(x, t, p)
+print("Hessian fwd(fwd) : ", hess_fwd, sep = "\n")
+hess_rev = jax.jacrev(jax.jacrev(objective, argnums = (0, 1, 2)), argnums = (0, 1, 2))(x, t, p)
+print("Hessian rev(rev) : ", hess_rev, sep = "\n")
